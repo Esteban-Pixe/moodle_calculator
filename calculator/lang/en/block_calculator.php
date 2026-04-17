c5b33028e94ca2eb83d973dc4a7c1490efbac6eb
Esteban-Pixels <stebannaranjo01@gmail.com>
2026-04-17 23:04:38 +0200

    Delete calculator/lang/en directory

--- calculator/lang/en/block_calculator.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('MOODLE_INTERNAL') || die();
-
-$string['pluginname'] = 'Calculator';
-$string['calculate'] = 'Calculate';
-$string['result'] = 'Result: {$a}';
-$string['divisionbyzero'] = 'Division by zero!';
-$string['invalidoperation'] = 'Invalid operation!';
